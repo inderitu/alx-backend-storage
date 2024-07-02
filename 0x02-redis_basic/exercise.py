0b5838486a5134182eea4dd2a9e7b2258c49b8dc
Innocent-Bern <innocentbern22@gmail.com>
2024-07-03 01:03:15 +0300

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python3
-
 """
-Create a Cache class. In the __init__ method, store an instance of the Redis
-client as a private variable named _redis (using redis.Redis()) and flush the
-instance using flushdb.
-
-Create a store method that takes a data argument and returns a string.
-The method should generate a random key (e.g. using uuid),
-store the input data in Redis using the random key and return the key.
-
-Type-annotate store correctly.
-Remember that data can be a str, bytes, int or float.
+redis db module
 """
 
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -34,3 +24,33 @@ class Cache:
         random_key = str(uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        takes a key string a argument and optional Callable argument named fn.
+        This callable will be used to convert the data back to the desired
+        format.
+        """
+        data = self._redis.get(key)
+        if fn:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        parametrize Cache.get with the correct conversion function
+        """
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """
+        parametrize Cache.get with the correct conversion function
+        """
+        data = self._redis.get(key)
+        try:
+            data = int(data.decode("utf-8"))
+        except Exception:
+            data = 0
+        return data
