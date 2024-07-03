a73163b55d3de6420a77afd6ddc9351cd3037c1f
Innocent-Bern <innocentbern22@gmail.com>
2024-07-03 10:25:39 +0300

    quick-fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -35,6 +35,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         generate a random key (using uuid)
